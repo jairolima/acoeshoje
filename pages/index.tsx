db9699e20d55ca43261b784eba30777c22ff3b84
Jairo Lima <msgjairo@gmail.com>
2020-12-03 03:49:29 -0300

    clean

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,39 @@
-import { Heading, Grid, Flex, Box, Stat, Icon,
+import {
+  Heading, Grid, Flex, Box, Stat, Icon,
   StatLabel,
   StatNumber,
   StatHelpText,
   StatArrow,
-  StatGroup, Button, Slide, Skeleton, useDisclosure} from "@chakra-ui/react";
+  StatGroup, Button, Slide, Skeleton, useDisclosure
+} from "@chakra-ui/react";
 
-  import css from '@styled-system/css'
+import css from '@styled-system/css'
 
-  import { ArrowUpIcon, ArrowDownIcon } from '@chakra-ui/icons'
+import { ArrowUpIcon, ArrowDownIcon } from '@chakra-ui/icons'
 
-  import { useFetch } from '../hooks/useFetch'
+import { useFetch } from '../hooks/useFetch'
 
-  declare module B3 {
+declare module B3 {
 
-    export interface Ibovespa {
-        cotacao: string;
-        porcentagem: string;
-    }
+  export interface Ibovespa {
+    cotacao: string;
+    porcentagem: string;
+  }
 
-    export interface ITSA4 {
-        cotacao: string;
-        porcentagem: string;
-    }
+  export interface ITSA4 {
+    cotacao: string;
+    porcentagem: string;
+  }
 
-    export interface RootObject {
-        ibovespa: Ibovespa;
-        ITSA4: ITSA4;
-    }
+  export interface RootObject {
+    ibovespa: Ibovespa;
+    ITSA4: ITSA4;
+  }
 
 }
 
 const Home: React.FC = () => {
-  
+
   const { data } = useFetch<B3.RootObject>('/b3')
 
   return (
@@ -52,69 +54,69 @@ const Home: React.FC = () => {
         alignItems="center"
       >
         <Flex gridArea="acoes" flexDir="row" justifyContent="space-between" alignItems="flex-center">
-         <Box>
-           <Heading size="2xl" lineHeight="shorter" fontWeight="bold" color="black.800">IBOVESPA</Heading>
-           <Heading size="sm" lineHeight="shorter" fontWeight="normal" color="black.800"></Heading>
-         </Box>
+          <Box>
+            <Heading size="2xl" lineHeight="shorter" fontWeight="bold" color="black.800">IBOVESPA</Heading>
+            <Heading size="sm" lineHeight="shorter" fontWeight="normal" color="black.800"></Heading>
+          </Box>
 
           <Flex flexDir="row">
 
 
-          {!data ? <Skeleton height="60px" width="30px"/> : 
-            <>
-          {data?.ibovespa.porcentagem.indexOf('+') > -1 ? 
-            <ArrowUpIcon mt={3} mr={3} bg="#48BB78" lineHeight="shorter" boxSize="42px" color="white" />
-            : 
-            <ArrowDownIcon mt={3} mr={3} bg="#F56565" lineHeight="shorter" boxSize="42px" color="white" />
-            }
-            </>
+            {!data ? <Skeleton height="60px" width="30px" /> :
+              <>
+                {data?.ibovespa.porcentagem.indexOf('+') > -1 ?
+                  <ArrowUpIcon mt={3} mr={3} bg="#48BB78" lineHeight="shorter" boxSize="42px" color="white" />
+                  :
+                  <ArrowDownIcon mt={3} mr={3} bg="#F56565" lineHeight="shorter" boxSize="42px" color="white" />
+                }
+              </>
             }
 
-          <Box>
-          <Heading size="2xl" lineHeight="shorter" fontWeight="bold" color="black.400">
-            
-          {!data ? 
-          <Skeleton height="60px" width="200px" /> : 
-          <Box css={css({
-            color: '#000',
-            animation: 'change 3s step-end both',
-          })}
-          >
-          {data?.ibovespa.cotacao}
-          </Box>
-          }
+            <Box>
+              <Heading size="2xl" lineHeight="shorter" fontWeight="bold" color="black.400">
 
-          </Heading>
-          <Heading size="sm" ml={2} lineHeight="shorter" fontWeight="normal" color="black.800">Pontos</Heading>
-          </Box>
-     
+                {!data ?
+                  <Skeleton height="60px" width="200px" /> :
+                  <Box css={css({
+                    color: '#000',
+                    animation: 'change 3s step-end both',
+                  })}
+                  >
+                    {data?.ibovespa.cotacao}
+                  </Box>
+                }
+
+              </Heading>
+              <Heading size="sm" ml={2} lineHeight="shorter" fontWeight="normal" color="black.800">Pontos</Heading>
+            </Box>
+
           </Flex>
           <Box>
-      
-            
-            {!data ? <Skeleton height="60px" width="100px"/> : 
 
-            <>
-            {data?.ibovespa.porcentagem.indexOf('+') > -1 ? 
-            <Heading animation="fade" size="2xl" lineHeight="shorter" fontWeight="bold" color="green.400">
-            {data?.ibovespa.porcentagem}
-            </Heading>
-            : 
-            <Heading animation="fade" size="2xl" lineHeight="shorter" fontWeight="bold" color="red.400">
-            {data?.ibovespa.porcentagem}
-            </Heading>
+
+            {!data ? <Skeleton height="60px" width="100px" /> :
+
+              <>
+                {data?.ibovespa.porcentagem.indexOf('+') > -1 ?
+                  <Heading animation="fade" size="2xl" lineHeight="shorter" fontWeight="bold" color="green.400">
+                    {data?.ibovespa.porcentagem}
+                  </Heading>
+                  :
+                  <Heading animation="fade" size="2xl" lineHeight="shorter" fontWeight="bold" color="red.400">
+                    {data?.ibovespa.porcentagem}
+                  </Heading>
+                }
+              </>
             }
-            </>
-            }
-            
-          <Heading size="sm" ml={2} lineHeight="shorter" fontWeight="normal" color="gray.600">variação (hoje)</Heading>
+
+            <Heading size="sm" ml={2} lineHeight="shorter" fontWeight="normal" color="gray.600">variação (hoje)</Heading>
           </Box>
         </Flex>
-      
-      
 
 
-{/* 
+
+
+        {/* 
         <Flex gridArea="top" flexDir="row" justifyContent="space-around" alignItems="flex-center">
 
           <Flex flexDir="column" maxW="50%">
